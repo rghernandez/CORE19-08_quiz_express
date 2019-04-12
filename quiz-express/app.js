ad77d12025a4ba72d8ade8e56c7cca18a797ca55
rghernandez <rgarciah12@gmail.com>
2019-04-12 12:56:02 +0200

    Express-partials installed

--- quiz-express/app.js
+++ quiz-express/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var partials = require('express-partials');
+
 
 var indexRouter = require('./routes/index');
 
@@ -17,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(partials());
 app.use('/', indexRouter);
 
 
